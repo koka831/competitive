caf82c9624fae28e9102755267c3402652a2f14f
koka <koka.code@gmail.com>
2018-04-07 20:50:11 +0900

    init

---
+++ atcoder/abc093/src/b.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn read<T>() -> Vec<T>
+where T:
+    std::str::FromStr,
+    T::Err: std::fmt::Debug {
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split(' ')
+        .map(|s| s.trim().parse().unwrap())
+        .collect()
+}
+
+fn read_one<T>() -> T
+where T:
+    std::str::FromStr,
+    T::Err: std::fmt::Debug {
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse().unwrap()
+}
