550f7e253f148f88d2e4958fe51e97836480583c
koka <koka.code@gmail.com>
2018-04-23 15:24:40 +0900

    add

---
+++ atcoder/top10/src/c.rs
@@ -0,0 +1,30 @@
+use std::io;
+#[allow(dead_code)]
+fn read<T>() -> Vec<T>
+where T:
+    std::str::FromStr,
+    T::Err: std::fmt::Debug {
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split_whitespace()
+        .map(|s| s.trim().parse().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_one<T>() -> T
+where T:
+    std::str::FromStr,
+    T::Err: std::fmt::Debug {
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse().unwrap()
+}
+
+fn main() {
+    let pat = ["eraser", "erase", "dreamer", "dream"];
+    let s = pat.iter().fold(read_one::<String>(), |s, x| s.replace(x, ""));
+    println!("{:?}", s);
+}
