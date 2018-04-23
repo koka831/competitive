fd2ac09956991d32769ef14320278a0e22a4f334
koka <koka.code@gmail.com>
2018-04-23 15:54:38 +0900

    add

---
+++ atcoder/lib/src/binary_search.rs
@@ -0,0 +1,44 @@
+use std::cmp;
+
+
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, &T) -> usize;
+    fn upper_bound(&self, &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                cmp::Ordering::Less => {
+                    low = mid + 1;
+                },
+                _ => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                cmp::Ordering::Greater => {
+                    high = mid;
+                },
+                _ => {
+                    low = mid + 1;
+                }
+            }
+        }
+        low
+    }
+}
---
+++ atcoder/lib/src/lib.rs
@@ -0,0 +1,52 @@
+use std::cmp;
+
+
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, &T) -> usize;
+    fn upper_bound(&self, &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                cmp::Ordering::Less => {
+                    low = mid + 1;
+                },
+                _ => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                cmp::Ordering::Greater => {
+                    high = mid;
+                },
+                _ => {
+                    low = mid + 1;
+                }
+            }
+        }
+        low
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
