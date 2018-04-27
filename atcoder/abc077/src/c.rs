619fb03bcd68528ff8c57087d1146375cf8c2e46
koka <koka.code@gmail.com>
2018-04-27 19:41:14 +0900

    wip: check how it works

--- atcoder/abc077/src/c.rs
+++ atcoder/abc077/src/c.rs
@@ -30,14 +30,49 @@ where T:
 }
 
 pub trait BinarySearch<T> {
-
+    fn lower_bound(&self, &T) -> usize;
+    fn upper_bound(&self, &T) -> usize;
 }
 
 impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
 
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                cmp::Ordering::Less => {
+                    low = mid + 1;
+                },
+                _ => { high = mid; }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                cmp::Ordering::Greater => {
+                    high = mid;
+                },
+                _ => { low = mid + 1; }
+            }
+        }
+        low
+    }
 }
 
 fn main() {
-    let n = read_one::<usize>();
+    // let n = read_one::<usize>();
 
+
+    let a = [1, 2, 3, 4, 5, 6, 7, 8];
+    println!("{}", a.lower_bound(&2));
+    println!("{}", a.upper_bound(&2));
 }
