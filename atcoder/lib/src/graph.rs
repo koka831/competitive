2d6025d658c990ccc24078d38d17ffa74e4de505
koka <koka.code@gmail.com>
2018-04-27 19:50:17 +0900

    feat: add breadth first search
    ref: ifcpc wata

---
+++ atcoder/lib/src/graph.rs
@@ -0,0 +1,18 @@
+use std::collections;
+
+pub fn bfs(g: &Vec<Vec<usize>>, s: usize) -> Vec<usize> {
+    let n = g.len();
+    let mut dist = vec![n + 1; n];
+    let mut que = colletions::VecDeque::new();
+    dist[s] = 0;
+    que.push_back(s);
+    while let Some(u) = que.pop_front() {
+        for &v in &g[u] {
+            if dist[v] == n + 1 {
+                dist[v] = dist[u] + 1;
+                que.push_back(v);
+            }
+        }
+    }
+    dist
+}
