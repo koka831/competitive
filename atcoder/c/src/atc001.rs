b166afa51da65a56f4d36b47f08d83ede7acd0ae
koka <koka.code@gmail.com>
2018-05-03 23:53:43 +0900

    solve: Atcoder Typical Problem

--- atcoder/c/src/atc001.rs
+++ atcoder/c/src/atc001.rs
@@ -27,6 +27,38 @@ where T:
     buf.trim().parse().unwrap()
 }
 
-fn main() {
+fn check_bound(x: usize, y: usize, w: usize, h: usize) -> bool {
+    return (0 <= x && x < w) && (0 <= y && y < h)
+}
 
+type Pos = (usize, usize);
+
+fn dfs(f: &mut Vec<Vec<char>>, x: usize, y: usize) -> bool {
+    f[x][y] = 's';
+
+    true
+}
+
+fn main() {
+    let (h, w) = {
+        let i = read::<usize>();
+        (i[0], i[1])
+    };
+
+    let mut vec = Vec::new();
+    for _ in 0..h {
+        vec.push(read::<char>());
+    }
+
+    for j in 0..h { for k in 0..w {
+        println!("{} {}", j, k);
+    }}
+
+    /*
+    if dfs(&vec, 0, 0) { 
+        println!("Yes"); 
+    } else { 
+        println!("No");
+    }
+    */
 }
