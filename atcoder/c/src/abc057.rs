7467db8ccb400ec16d5e6d066e7f9a3c47b1fd0c
koka <koka.code@gmail.com>
2018-05-08 03:07:02 +0900

    ac

--- atcoder/c/src/abc057.rs
+++ atcoder/c/src/abc057.rs
@@ -1,8 +1,23 @@
 use std::io;
+use std::cmp;
 
 
+/// 1 < n < 10^10
+/// a * b = n
+/// min(max(digits of a, digits of b))
 fn main() {
+    let n = read_one::<usize>();
+    let ans = (1..).take_while(|&x| x * x <= n)
+        .filter(|&x| n % x == 0)
+        .map(|x| cmp::max(digits(x), digits(n / x)))
+        .min()
+        .unwrap();
 
+    println!("{}", ans);
+}
+
+fn digits(x: usize) -> usize {
+    x.to_string().len()
 }
 
 
