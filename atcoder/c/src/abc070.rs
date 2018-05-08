6d5526b3cc9959547394bc12d8a8eb054c32ff74
koka <koka.code@gmail.com>
2018-05-08 14:30:36 +0900

    ac

---
+++ atcoder/c/src/abc070.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+/// https://beta.atcoder.jp/contests/abc070/tasks/abc070_c
+fn main() {
+    let n = read_one::<u64>();
+    let ans = (0..n).fold(1, |a, _| lcm(a, read_one::<u64>()));
+    println!("{}", ans);
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    // avoid overflow
+    (a / gcd(a, b)) * b
+}
+
+
+#[allow(dead_code)]
+fn read<T>() -> Vec<T>
+where T:
+    std::str::FromStr,
+    T::Err: std::fmt::Debug {
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split_whitespace()
+        .map(|s| s.trim().parse().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_one<T>() -> T
+where T:
+    std::str::FromStr,
+    T::Err: std::fmt::Debug {
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse().unwrap()
+}
