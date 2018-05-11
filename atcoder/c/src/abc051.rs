50be9dd6502d40399dd452538d942bc59edc535b
koka <koka.code@gmail.com>
2018-05-11 21:32:22 +0900

    3wa: missing dx to dy

--- atcoder/c/src/abc051.rs
+++ atcoder/c/src/abc051.rs
@@ -1,41 +1,52 @@
 use std::io;
 
 
+/// https://beta.atcoder.jp/contests/abc051/tasks/abc051_c
 fn main() {
     let (sx, sy, gx, gy) = {
         let i = read::<isize>();
         (i[0], i[1], i[2], i[3])
     };
 
-    let mut buf = String::new();
+    let mut buf = MyStr::new();
     let dx = (gx - sx).abs();
     let dy = (gy - sy).abs();
-    // sx, sy ----> gx, sy
-    //   |             |
-    //   |             |
-    // sx, gy <---- gx, gy
-    // 1. sx -> gx
-    // 2. sy -> gy
-    // 3. gx -> sx
-    // 4. gy -> sy
-    for _ in 0..dx {
-        buf += &"U";
-    }
 
-    for _ in 0..dy {
-        buf += &"R";
-    }
-
-    for _ in 0..dx {
-        buf += &"D";
-    }
-
-    for _ in 0..dy {
-        buf += &"L";
-    }
+    buf.append("U", dy);
+    buf.append("R", dx); // (t_x, t_y)
+    buf.append("U", 1);
+    buf.append("L", dx + 1);
+    buf.append("D", dy + 1);
+    buf.append("R", 1); // (s_x, s_y)
+    buf.append("R", dx);
+    buf.append("U", dy); // (t_x, t_y)
+    buf.append("R", 1);
+    buf.append("D", dy + 1);
+    buf.append("L", dx + 1);
+    buf.append("U", 1); // (s_x, s_y)
 
     println!("{}", buf);
+}
 
+
+struct MyStr {
+    s: String,
+}
+
+impl MyStr {
+    fn new() -> MyStr {
+        MyStr { s: String::new(), }
+    }
+
+    fn append(&mut self, dir: &str, time: isize) {
+        for _ in 0..time { self.s += dir; }
+    }
+}
+
+impl std::fmt::Display for MyStr {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.s)
+    }
 }
 
 #[allow(dead_code)]
