49e4e2602ffa8a87a2c6e3864eb9e460d99a9f51
koka <koka.code@gmail.com>
2018-07-11 13:12:59 +0900

    unionfind

--- atcoder/c/src/bin/abc054.rs
+++ atcoder/c/src/bin/abc054.rs
@@ -1,8 +1,89 @@
 use std::io;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 
 
 fn main() {
+    let (n, m) = {
+        let i = read::<usize>();
+        (i[0], i[1])
+    };
 
+}
+
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    pos: usize,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &State) -> Ordering {
+        match other.cost.cmp(&self.cost) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Equal => {
+                self.pos.cmp(&other.pos)
+            }
+        }
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Edge {
+    node: usize,
+    cost: usize,
+}
+
+fn dijkstra(adj: &Vec<Vec<Edge>>, s: usize) -> Vec<usize> {
+    let mut dist = (0..adj.len()).map(|_| ::std::usize::MAX)
+        .collect::<Vec<usize>>();
+
+    let mut q = BinaryHeap::new();
+    dist[s] = 0;
+    q.push( State { cost: 0, pos: s });
+
+    while let Some( State { cost, pos }) = q.pop() {
+
+        if cost > dist[pos] { continue; }
+
+        for e in &adj[pos] {
+            let s = State { cost: cost + e.cost, pos: e.node };
+            if s.cost < dist[s.pos] {
+                q.push(s);
+                dist[s.pos] = s.cost;
+            }
+        }
+    }
+
+    dist
+}
+
+fn dfs(g: &Vec<Vec<bool>>, n: usize, s: usize) -> usize {
+    let mut visited = (0..n).map(|_| false)
+        .collect::<Vec<bool>>();
+    let mut q = BinaryHeap::new();
+    let mut cnt = 0;
+    visited[s] = true;
+    q.push(s);
+
+    while let Some(pos) = q.pop() {
+        if visited.iter().filter(|&v| *v == true).count() == n {
+            cnt += 1;
+        }
+
+        for e in &g[pos] {
+        }
+    }
+    cnt
 }
 
 
--- atcoder/c/src/bin/abc075.rs
+++ atcoder/c/src/bin/abc075.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::cmp;
+use std::collections::HashMap;
 
 
 /// https://beta.atcoder.jp/contests/abc075/tasks/abc075_c
@@ -7,6 +9,87 @@ fn main() {
         let i = read::<usize>();
         (i[0], i[1])
     };
+
+    let mut g = Vec::new();
+    for _ in 0..m {
+        let i = read::<usize>();
+        g.push((i[0] - 1, i[1] - 1));
+    }
+
+    let mut cnt = 0;
+
+    for j in 0..m {
+        let mut  g_clone = g.clone();
+        g_clone.remove(j);
+
+        let mut uf = UnionFind::new(n);
+        let mut edge = vec![0; n];
+
+        for (u, v) in g_clone {
+            uf.unite(u, v);
+            edge[u] += 1;
+            edge[v] += 1;
+        }
+
+        let mut hm = HashMap::new();
+
+        for k in 0..n {
+            hm.entry(uf.find(k))
+                .or_insert(Vec::new()).push(k);
+        }
+
+        // println!("{:?}", hm);
+        // println!("{:?}", edge);
+        if hm.len() > 1 { cnt += 1; }
+    }
+
+    println!("{}", cnt);
+}
+
+
+struct UnionFind {
+    par: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+
+impl UnionFind {
+    fn new(n: usize) -> UnionFind {
+        UnionFind {
+            par: (0..n).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.par[x] == x { x }
+        else {
+            let p = self.par[x];
+            let pp = self.find(p);
+            self.par[x] == pp;
+            pp
+        }
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.find(x);
+        let y = self.find(y);
+
+        if x == y { return; }
+
+        match self.rank[x].cmp(&self.rank[y]) {
+            cmp::Ordering::Equal => {
+                self.par[y] = x;
+                self.rank[x] += 1;
+            },
+            cmp::Ordering::Less => {
+                self.par[x] = y;
+            },
+            cmp::Ordering::Greater => {
+                self.par[y] = x;
+            }
+        }
+    }
 }
 
 
