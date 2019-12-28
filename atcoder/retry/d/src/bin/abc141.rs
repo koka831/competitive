dbeb391b3c0057e361b12013c1637ddfb320adfe
koka <koka.code@gmail.com>
2019-12-29 08:14:51 +0900

    add

--- atcoder/retry/d/src/bin/abc141.rs
+++ atcoder/retry/d/src/bin/abc141.rs
@@ -1,25 +1,12 @@
 use std::io;
-use std::cmp;
 
 
 fn main() {
-    let (h, w) = {
+    let (n, m) = {
         let i = read::<usize>();
         (i[0], i[1])
     };
-    let mut g = vec![Vec::new(); h];
-    for _ in 0..h { g.push(read_one::<String>().chars().collect::<Vec<_>>()); }
-    let mut l = vec![vec![0; w]; h];
-    let mut r = vec![vec![0; w]; h];
-    let mut u = vec![vec![0; w]; h];
-    let mut d = vec![vec![0; w]; h];
-
-    for i in 0..h { for j in 0..w {
-        if i > 0 {
-            if g[i][j] == '.' { l[i][j] = l[i][j - 1] + 1; }
-            else { l[i][j] = l[i][j]; }
-        }
-    }}
+    
 }
 
 
